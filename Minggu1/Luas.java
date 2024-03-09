94f0f1823d0d350f7bd10a5d72a6f0115792622d
Naufal Arsyaputra Pradana <113005082+NaufalArsyaputraPradana@users.noreply.github.com>
2024-03-09 14:55:32 +0700

    Add files via upload

---
+++ Minggu1/Luas.java
@@ -0,0 +1,17 @@
+package Minggu1;
+
+public class Luas {
+
+	public static void main(String[] args) {
+		System.out.println("Luas Segitiga");
+		System.out.println("Rumus : (alas x tinggi):2 ");
+		System.out.println("alas = 6");
+		System.out.println("tinggi = 8");
+		int alas = 6;
+		int tinggi = 8;
+		int hasil = (alas*tinggi)/2;
+		System.out.print("hasil = ");
+		System.out.print(hasil);
+	}
+
+}
---
+++ Minggu1/Perkenalan.java
@@ -0,0 +1,11 @@
+package Minggu1;
+
+public class Perkenalan {
+
+	public static void main(String[] args) {
+		System.out.println("Halo, Nama Saya Adalah Naufal Arsyaputra Pradana");
+		System.out.println("Saya Adalah Mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+		System.out.println("Saya Sedang Menulis Program Java");
+	}
+
+}
